0a6d7561521dff3424c9e22dd0d37605fe9f90ba
박성현 <pjhomg5530@naver.com>
2023-05-08 21:12:04 +0900

    :fire: :: 주석 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id(Dependency.Gradle.APPLICATION) version Versions.GRADLE apply false
     id(Dependency.Gradle.LIBRARY) version Versions.GRADLE apply false
