58d8c15f11c4c7d0eedd952eb17db17d9a29b4c9
박성현 <pjhomg5530@naver.com>
2023-05-09 10:41:27 +0900

    :fire: :: domain에 불 필요한 코드 삭제하기

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -32,9 +32,6 @@ android {
 }
 
 dependencies {
-    implementation(Dependency.AndroidX.CORE_KTX)
-    implementation(Dependency.AndroidX.APPCOMPAT)
-    implementation(Dependency.Google.MATERIAL)
     testImplementation(Dependency.Test.JUNIT)
     androidTestImplementation(Dependency.Test.ANDROID_JUNIT)
     androidTestImplementation(Dependency.Test.ESPRESSO)
