1f5d8314d9a2ebf6a5bf74aaf65dd3ba889aaa1b
박성현 <pjhomg5530@naver.com>
2023-05-16 15:39:20 +0900

    :sparkles: :: Hilt 멘션 추가하기

--- app/src/main/java/team/msg/hi_v2/MainActivity.kt
+++ app/src/main/java/team/msg/hi_v2/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import team.msg.hi_v2.theme.Hiv2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
