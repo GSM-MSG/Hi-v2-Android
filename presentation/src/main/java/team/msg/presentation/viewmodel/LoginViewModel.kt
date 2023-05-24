cf155f0ed7477238a7563dc458a2398c2861e1a1
박성현 <pjhomg5530@naver.com>
2023-05-24 11:02:03 +0900

    :sparkles: ::  LoginViewModel 만들기

---
+++ presentation/src/main/java/team/msg/presentation/viewmodel/LoginViewModel.kt
@@ -0,0 +1,18 @@
+package team.msg.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import team.msg.domain.model.auth.request.GAuthLoginRequestData
+import team.msg.domain.usecase.GAuthLoginUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val gAuthLoginUseCase: GAuthLoginUseCase
+): ViewModel() {
+    fun gAuthLogin(code: String) = viewModelScope.launch {
+        gAuthLoginUseCase(GAuthLoginRequestData(code))
+    }
+}
