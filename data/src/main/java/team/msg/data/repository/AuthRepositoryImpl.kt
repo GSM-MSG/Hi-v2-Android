888c6a9d9e1fb566e04e1ae6ea228f819494c624
박성현 <pjhomg5530@naver.com>
2023-05-26 10:57:36 +0900

    :sparkles: :: AuthRepository에 토큰 저장하기 기능 만들기

--- data/src/main/java/team/msg/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/team/msg/data/repository/AuthRepositoryImpl.kt
@@ -2,6 +2,7 @@ package team.msg.data.repository
 
 import team.msg.data.dto.auth.request.GAuthLoginRequest
 import team.msg.data.dto.auth.response.toLoginResponseData
+import team.msg.data.local.datasource.auth.LocalAuthDataSource
 import team.msg.data.remote.datasource.auth.AuthDataSource
 import team.msg.domain.model.auth.request.GAuthLoginRequestData
 import team.msg.domain.model.auth.response.GAuthLoginResponseData
@@ -9,8 +10,18 @@ import team.msg.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-  private val  authDataSource: AuthDataSource
+  private val authDataSource: AuthDataSource,
+  private val localAuthDataSource: LocalAuthDataSource
 ): AuthRepository {
     override suspend fun gAuthLogin(body: GAuthLoginRequestData): GAuthLoginResponseData =
         authDataSource.gAuthLogin(GAuthLoginRequest(body.code)).toLoginResponseData()
+
+    override suspend fun saveTokenInfo(
+        accessToken: String,
+        refreshToken: String,
+        accessExp: String,
+        refreshExp: String
+    ) {
+        localAuthDataSource.saveTokenInfo(accessToken, refreshToken, accessExp, refreshExp)
+    }
 }
