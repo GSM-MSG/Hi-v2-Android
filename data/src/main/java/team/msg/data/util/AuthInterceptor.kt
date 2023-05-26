de7ae7955d4dc559e354465b37353b3ab2af26d8
박성현 <pjhomg5530@naver.com>
2023-05-26 11:20:09 +0900

    :recycle: ::  시간 저장 방식 바꾸기

--- data/src/main/java/team/msg/data/util/AuthInterceptor.kt
+++ data/src/main/java/team/msg/data/util/AuthInterceptor.kt
@@ -1,5 +1,6 @@
 package team.msg.data.util
 
+import android.util.Log
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import kotlinx.coroutines.flow.first
@@ -13,22 +14,21 @@ import okhttp3.Response
 import team.msg.data.BuildConfig
 import team.msg.data.local.datasource.auth.LocalAuthDataSource
 import team.msg.domain.exception.NeedLoginException
-import java.text.SimpleDateFormat
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
     private val dataSource: LocalAuthDataSource
-): Interceptor {
+) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        val builder =  request.newBuilder()
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
-        val currentTime = dateFormat.format(System.currentTimeMillis()).toString()
+        val builder = request.newBuilder()
         val ignorePath = listOf("/auth")
         val ignoreMethod = listOf("POST")
         val path = request.url.encodedPath
         val method = request.method
+        val currentTime = LocalDateTime.now()
 
         ignorePath.forEachIndexed { index, s ->
             if (s == path && ignoreMethod[index] == method) {
@@ -37,17 +37,21 @@ class AuthInterceptor @Inject constructor(
         }
 
         runBlocking {
-            val accessExp = dataSource.getAccessExp().first()
-            val refreshExp = dataSource.getRefreshExp().first()
+            val accessExp = LocalDateTime.parse(dataSource.getAccessExp().first())
+            val refreshExp = LocalDateTime.parse(dataSource.getRefreshExp().first())
+            val refreshToken = dataSource.getRefreshToken().first()
+            Log.d("intercept", "refreshToken: $refreshToken")
+            Log.d("intercept", "accessExp: $accessExp")
+            Log.d("intercept", "refreshExp: $refreshExp")
 
-            if (refreshExp >= currentTime) throw NeedLoginException()
+            if (currentTime.isAfter(refreshExp)) throw NeedLoginException()
 
-            if (accessExp >= currentTime) {
+            if (currentTime.isAfter(accessExp)) {
                 val client = OkHttpClient()
                 val refreshRequest = Request.Builder()
                     .url(BuildConfig.BASE_URL + "auth")
                     .patch("".toRequestBody("application/json".toMediaTypeOrNull()))
-                    .addHeader("RefreshToken","Bearer ${dataSource.getRefreshToken().first()}")
+                    .addHeader("RefreshToken", "Bearer $refreshToken")
                     .build()
                 val jsonParser = JsonParser()
                 val response = client.newCall(refreshRequest).execute()
@@ -57,7 +61,7 @@ class AuthInterceptor @Inject constructor(
                     dataSource.setRefreshToken(token["refreshToken"].toString())
                     dataSource.setAccessExp(token["accessExpiredAt"].toString())
                     dataSource.setRefreshExp(token["refreshExpiredAt"].toString())
-                } else throw  NeedLoginException()
+                } else throw NeedLoginException()
             }
             builder.addHeader("Authorization", "Bearer ${dataSource.getAccessToken().first()}")
         }
