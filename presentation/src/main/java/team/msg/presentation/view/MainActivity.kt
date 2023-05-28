1b385b29f3e91ac073aac4335c3c5758bc3b60f8
박성현 <pjhomg5530@naver.com>
2023-05-28 16:16:38 +0900

    :fire: ::  안 쓰이는 import 지우기

--- presentation/src/main/java/team/msg/presentation/view/MainActivity.kt
+++ presentation/src/main/java/team/msg/presentation/view/MainActivity.kt
@@ -3,30 +3,7 @@ package team.msg.presentation.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.msg.gauthsignin.GAuthSigninWebView
-import com.msg.gauthsignin.component.GAuthButton
-import com.msg.gauthsignin.component.utils.Types
 import dagger.hilt.android.AndroidEntryPoint
-import team.msg.presentation.BuildConfig
-import team.msg.presentation.R
-import team.msg.presentation.viewmodel.LoginViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
