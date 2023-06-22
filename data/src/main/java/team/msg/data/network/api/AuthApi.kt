e8c5166d10c1eb93e082832dbbf253b8f1bc5598
박성현 <pjhomg5530@naver.com>
2023-06-22 17:50:22 +0900

    :sparkles: ::  getGAuthAuthLink, logout API 추가

--- data/src/main/java/team/msg/data/network/api/AuthApi.kt
+++ data/src/main/java/team/msg/data/network/api/AuthApi.kt
@@ -1,6 +1,9 @@
 package team.msg.data.network.api
 
 import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import team.msg.data.dto.auth.request.GAuthLoginRequest
 import team.msg.data.dto.auth.response.GAuthLoginResponse
@@ -10,4 +13,10 @@ interface AuthApi {
     suspend fun gAuthLogin(
         @Body body: GAuthLoginRequest
     ): GAuthLoginResponse
+
+    @GET("auth")
+    suspend fun getGAuthAuthLink(): String
+
+    @DELETE("auth")
+    suspend fun logout()
 }
