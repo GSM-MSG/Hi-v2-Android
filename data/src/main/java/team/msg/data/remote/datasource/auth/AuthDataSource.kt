bc8ee38d2a6dd4eea4803f9868ab3c1b411efeed
박성현 <pjhomg5530@naver.com>
2023-06-22 20:36:28 +0900

    :sparkles: :: AuthDataSource에 api 추가

--- data/src/main/java/team/msg/data/remote/datasource/auth/AuthDataSource.kt
+++ data/src/main/java/team/msg/data/remote/datasource/auth/AuthDataSource.kt
@@ -1,8 +1,13 @@
 package team.msg.data.remote.datasource.auth
 
 import team.msg.data.dto.auth.request.GAuthLoginRequest
+import team.msg.data.dto.auth.response.GAuthAuthLinkResponse
 import team.msg.data.dto.auth.response.GAuthLoginResponse
 
 interface AuthDataSource {
     suspend fun gAuthLogin(body: GAuthLoginRequest): GAuthLoginResponse
+
+    suspend fun gAuthAuthLink(): GAuthAuthLinkResponse
+
+    suspend fun logout()
 }
