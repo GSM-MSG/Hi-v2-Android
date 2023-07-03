c5df511341e12189089ddb0a86775271c8561c73
박성현 <pjhomg5530@naver.com>
2023-07-03 09:02:19 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/team/msg/data/dto/auth/response/GAuthAuthLinkResponse.kt
+++ data/src/main/java/team/msg/data/dto/auth/response/GAuthAuthLinkResponse.kt
@@ -1,8 +1,13 @@
 package team.msg.data.dto.auth.response
 
 import com.google.gson.annotations.SerializedName
+import team.msg.domain.model.auth.response.GAuthAuthLinkResponseData
 
 data class GAuthAuthLinkResponse(
     @SerializedName("authLink")
     val authLink: String
+)
+
+fun GAuthAuthLinkResponse.asGAuthAuthLinkResponseData() = GAuthAuthLinkResponseData(
+    authLink = authLink
 )
