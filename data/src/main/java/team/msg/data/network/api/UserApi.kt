12557fe4aa71cd85af6224a5612aa4382510ee44
박성현 <pjhomg5530@naver.com>
2023-07-04 18:18:07 +0900

    :recycle: :: 새롭게 폴더링(분리)하고, 이름 변경한 model 적용

--- data/src/main/java/team/msg/data/network/api/UserApi.kt
+++ data/src/main/java/team/msg/data/network/api/UserApi.kt
@@ -4,28 +4,28 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.Path
-import team.msg.data.dto.user.request.SearchStudentRequest
-import team.msg.data.dto.user.request.StudentStatusRequest
-import team.msg.data.dto.user.response.SearchStudentResponse
-import team.msg.data.dto.user.response.StudentResponse
-import team.msg.data.dto.user.response.UserResponse
+import team.msg.data.dto.user.request.getUserInfo.GetUserInfoRequest
+import team.msg.data.dto.user.request.modifyStudentStatus.ModifyStudentStatusRequest
+import team.msg.data.dto.user.response.getUserInfo.GetUserInfoResponse
+import team.msg.data.dto.user.response.getAllStudentList.GetAllStudentListResponse
+import team.msg.data.dto.user.response.getMyPageInfo.GetMyPageInfoResponse
 import java.util.UUID
 
 interface UserApi {
     @GET("user")
-    suspend fun getMyPageInfo(): UserResponse
+    suspend fun getMyPageInfo(): GetMyPageInfoResponse
 
     @GET("user/student")
-    suspend fun getAllStudentList(): List<StudentResponse>
+    suspend fun getAllStudentList(): List<GetAllStudentListResponse>
 
     @PATCH("user/{id}")
     suspend fun modifyStudentStatus(
         @Path("id") id: UUID,
-        @Body body: StudentStatusRequest
+        @Body body: ModifyStudentStatusRequest
     )
 
     @GET("user/search")
     suspend fun getUserInfo(
-        @Body body: SearchStudentRequest
-    ): SearchStudentResponse
+        @Body body: GetUserInfoRequest
+    ): GetUserInfoResponse
 }
